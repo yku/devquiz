2610c46ac1eba98a1357515afaecb4d5c1ca1863
yku <kz2yz4@gmail.com>
2011-09-07 17:09:45 +0900

    Change algorithm to Astar

--- gdd2011jp/slidepuzzle/slidepuzzle.cc
+++ gdd2011jp/slidepuzzle/slidepuzzle.cc
@@ -1,18 +1,52 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
 #include <cstring>
+#include <cstdio>
 #include <ctime>
 #include <algorithm>
 #include <queue>
 #include <map>
+#include <set>
+#include <iomanip>
 
-#define _DEBUG_ 0
-#define _TIME_  1
+#define _DEBUG_    0
+#define _SCORE_    1
+#define _SKIP_     0
+
+#define TIMEOUT   30
+#define MAX_WIDTH  6
+#define MAX_HEIGHT 6
 
 using namespace std;
 
 long LX, RX, UX, DX;
+
+int answers[MAX_WIDTH][MAX_HEIGHT] = { 0 };
+int questions[MAX_WIDTH][MAX_HEIGHT] = { 0 };
+
+typedef struct {
+    string state, route;
+    int depth;
+    int cost;
+    char dir;
+} NODE;
+
+typedef struct {
+    string route;
+    int cost;
+    char dir;
+} PARM;
+
+struct cmp {
+    bool operator() (NODE lhs, NODE rhs) { return lhs.cost > rhs.cost; }
+};
+
+int GetSpacePos(string b) {
+    return b.find("0");
+}
+
 string GetFinalState(string bi)
 {
     static const char t[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A',
@@ -29,17 +63,16 @@ string GetFinalState(string bi)
     return bf;
 }
 
-// $B;vA0$K:n$C$?$[$&$,$h$$(B
 void CreateAdjList(int w, int h, int adj[][5])
 {
-    memset(adj, -1, w * h * 5 * sizeof(int)); // 4$B$G$O$J$/(B5$B$J$N$OHVJ<(B
+    memset(adj, -1, w * h * 5 * sizeof(int)); // 4ではなく5なのは番兵
  
     for(int i = 0; i < w * h; i++) {
         int j = 0;
-        // $B2#J}8~$NNY@\%A%'%C%/(B
+        // 横方向の隣接チェック
         if(i % w - 1 > -1) adj[i][j++] = i - 1;
         if(i % w + 1 < w)  adj[i][j++] = i + 1;
-        // $B=DJ}8~$NNY@\%A%'%C%/(B
+        // 縦方向の隣接チェック
         if(i - w > -1) adj[i][j++] = i - w;
         if(i + w < w * h) adj[i][j++] = i + w;
     }
@@ -54,25 +87,6 @@ void CreateAdjList(int w, int h, int adj[][5])
 #endif
 }
 
-int GetSpacePos(string b)
-{
-    for(int i = 0; i < b.size(); i++) {
-        if(b[i] == '0'){
-            return i;
-        }
-    }
-    return -1;
-}
-
-void PrintBoard(int w, int h, string b)
-{
-    for(int i = 0; i < b.size(); i++) {
-        cout << b[i];
-        if((i+1) % w == 0) cout << endl;
-    }
-    cout << endl;
-}
-
 string GetOperation(const int n, const int sp, const int w) 
 {
     string ret = "";
@@ -82,6 +96,20 @@ string GetOperation(const int n, const int sp, const int w)
     else if(n == (sp - w)) ret = "U";
     else if(n == (sp + w)) ret = "D";
     
+    return ret;
+}
+
+string ReverseOperation(string op)
+{
+    string ret = "";
+    for(int i = 0; i < op.size(); i++) {
+        switch(op[i]) {
+            case 'L': ret += "R"; break;
+            case 'R': ret += "L"; break;
+            case 'U': ret += "D"; break;
+            case 'D': ret += "U"; break;
+        }
+    }
     return ret;
 }
 
@@ -104,88 +132,209 @@ bool DecUsedOperation(string op)
     return false;
 }
 
-string Search(int w, int h, string bi, string bf, int adj[][5])
+void PrintBoard(int w, int h, string b)
 {
-    queue<string> q;
-    map<string, string> visited;
-    q.push(bi);
-    visited.insert(pair<string, string>(bi, ""));
+    for(int i = 0; i < b.size(); i++) {
+        cout << b[i];
+        if((i+1) % w == 0) cout << endl;
+    }
+}
 
-    while(!q.empty()) {
-        string state = q.front();
-        q.pop();
-        int space_pos = GetSpacePos(state);
+int CalculateCost(int w, int h, string b1, string b2)
+{
+    int ret = 0;
+    for(int i = 0; i < b1.size(); i++) {
+        if(b1[i] == '0' or b1[i] == '=') continue;
+        int target = b2.find(b1[i]);
+        int x = i % w;
+        int y = i / w;
+        int xx = target % w;
+        int yy = target / w;
+        
+        if(x == xx and y == yy) continue;
+        ret += abs(xx - x) + abs(yy - y);
+        
+        if(x == xx) {
+            // equal col
+            if(y < yy) {
+                for(int j = i; j < target; j += w) {
+                    if(b1[j] == '=') { ret += 2; break; }
+                }
+            } else {
+                for(int j = i; j > target; j -= w) {
+                    if(b1[j] == '=') { ret += 2; break; }
+                }
+            }
+        } else if(y == yy) {
+            // equal row
+            if(x < xx) {
+                for(int j = i; j < target; j++) {
+                    if(b1[j] == '=') { ret += 2; break; }
+                }
+            } else {
+                for(int j = i; j > target; j--) {
+                    if(b1[j] == '=') { ret += 2; break; }
+                }
+            }
+        } 
+#if _DEBUG_
+        int t = 0;
+        t = abs(xx - x) + abs(yy - y);
+        cout << t << " ";
+        if((i+1) % w == 0) cout << endl;
+#endif
+    }
+
+    return ret;
+}
+
+string AstarSearch(int w, int h, string bi, string bf, int adj[][5])
+{
+    string ret = "";
+    NODE init, goal;
+    priority_queue<NODE, vector<NODE>, cmp> open;
+    map<string, PARM> close;
+    bool clear = false;
+    clock_t stime = clock();
+
+    init.state = bi;
+    init.route = "";
+    init.depth = 0;
+    init.cost  = 0;
+    init.dir   = 'F';
+    open.push(init);
+    
+    goal.state = bf;
+    goal.route = "";
+    goal.depth = 0;
+    goal.cost  = 0;
+    goal.dir   = 'B';
+    open.push(goal);
+    while(!open.empty()) {
+        NODE node = open.top();
+        string target;
+        PARM parm;
+        clock_t etime = clock();
+        if((etime - stime) / CLOCKS_PER_SEC > TIMEOUT) { return ""; } 
+        open.pop();
+        if(node.dir == 'F') target = bf;
+        else                target = bi;
+        parm.route = node.route;
+        parm.cost = CalculateCost(w, h, node.state, target) + node.depth;
+        parm.dir = node.dir;
+        close.insert(pair<string, PARM>(node.state, parm));
+        if(node.state == bf and clear) { 
+            DecUsedOperation(node.route);
+            return node.route; 
+        }
+        int space_pos = node.state.find("0");
         int n;
         for(int i = 0; (n = adj[space_pos][i]) != -1; i++) {
-            if(state[n] == '=') continue;
-            string next_state(state);
-            next_state[space_pos] = state[n];
+            if(node.state[n] == '=') continue;
+            string next_state(node.state);
+            string route;
+            
+            next_state[space_pos] = node.state[n];
             next_state[n] = '0';
-            string op = visited[state];
-            op += GetOperation(n, space_pos, w);
+            
+            if(close.count(next_state) != 0) {
+                if(close[next_state].dir != node.dir) {
+                    string f_route, b_route;
+                    
+                    if(close[next_state].dir == 'B') {
+                        f_route = node.route + GetOperation(n, space_pos, w);
+                        b_route = close[next_state].route;
+                    }else{
+                        f_route = close[next_state].route;
+                        b_route = node.route + GetOperation(n, space_pos, w);  
+                    }
+                    reverse(b_route.begin(), b_route.end());
+                    route = f_route + ReverseOperation(b_route);
+                    clear = true;
+                }else{
+                    int cost, prev_cost;
+                    cost = CalculateCost(w, h, next_state, target) + node.depth + 1;
+                    prev_cost = close[next_state].cost;
+                    if(cost >= prev_cost) { continue; }
+                    close.erase(next_state);
+                } 
 
-            if(next_state == bf) {
-                //PrintBoard(w, h, next_state);
-                if(DecUsedOperation(op)) return op;             
-                else                     return "";
-            }else if(visited.count(next_state) == 0) {
-                q.push(next_state);
-                visited.insert(pair<string, string>(next_state, op));
-/*
-#if _DEBUG_
-                PrintBoard(w, h, next_state);
-#endif
-*/
             }
+            
+            NODE next;
+            next.state = clear ? bf : next_state;
+            next.route = clear ? route : node.route + GetOperation(n, space_pos, w);
+            next.cost = clear ? 0 : CalculateCost(w, h, next_state, target) + node.depth + 1;
+            next.depth = node.depth + 1;
+            next.dir = node.dir;
+            open.push(next);
         }
     }
-    return "";
+    
+    return ret;
 }
 
 int main()
 {
     int N;
     scanf("%ld %ld %ld %ld %d", &LX, &RX, &UX, &DX, &N);
- 
-#if _TIME_
+
+#if _SKIP_
+    ifstream ifs("result.txt");
+    if(ifs.fail()) {
+        cerr << "Could not open file:result.txt\n";
+        exit(8);
+    }
+#endif
+
+
+#if _SCORE_
     long lx, rx, ux, dx;
     lx = LX;
     rx = RX;
     ux = UX;
     dx = DX;
-#endif
 
-#if _TIME_
     clock_t stime = clock();
 #endif
-    //N = 1;
     for(int i = 0; i < N; i++) {
         int wi, hi;
-        string s, bi, ret;
-        
+        string s, bi, bf, ret;
         cin >> s;
         sscanf(s.c_str(), "%d,%d,", &wi, &hi);
         bi = s.substr(4);
-        
-        //bi =  "168452=30";
-        //bi =  "867254301";
-        //bi =  "123405786";
-        //wi = hi = 3;
-        // $B$H$j$"$($:(B3x3$B$r$H$/$NL\I8$K(B
-        if(wi != 3 or hi != 3){ cout << endl;  continue; }
-        
+        questions[wi][hi]++;
         int buf[wi * hi][5];
-        //PrintBoard(wi, hi, bi);
         CreateAdjList(wi, hi, buf);
-        ret = Search(wi, hi, bi, GetFinalState(bi), buf);
+#if _RESUME_
+        string str = "";
+        getline(ifs, str);
+        if(str != ""){ 
+            cout << str << endl;
+            DecUsedOperation(str);
+            answers[wi][hi]++;
+            continue;
+        }
+#endif
+        bf = GetFinalState(bi);
+        ret = AstarSearch(wi, hi, bi, bf, buf); 
+        if(ret != "") answers[wi][hi]++;
         cout << ret << endl;
     }
-
-#if _TIME_
+#if _SCORE_
     clock_t etime = clock();
-    cout << "TIME:" << (etime - stime) << "[usec] " << endl;
+    int a = 0;
+    cout << "TIME:" << (etime - stime) / CLOCKS_PER_SEC << "." << (etime - stime) % CLOCKS_PER_SEC << "[sec] " << endl;
     cout << "OPERATION:" << LX << "/" << lx << " " << RX << "/" << rx << " " << UX << "/" << ux << " " << DX << "/" << dx << endl; 
     cout << "TOTAL USED:" << (lx - LX) + (rx - RX) + (ux - UX) + (dx - DX) << endl;
+    for(int h = 3; h <= MAX_HEIGHT; h++) {
+        for(int w = 3; w <= MAX_WIDTH; w++) {
+            cout << setw(4) << answers[w][h] << "/" << setw(4) << questions[w][h] << " ";
+            a += answers[w][h];
+        }
+        cout << endl;
+    }
+    cout << "ANSWERS:" << setw(4) << a << "/" << N << endl;
 #endif
     return 0;
 }
